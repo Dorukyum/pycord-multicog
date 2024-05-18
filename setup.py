7ec7b008e5c98c22b41138a1df5aeeb73baf3083
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2024-05-18 07:33:46 +0300

    Bump version to 2.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="pycord-multicog",
     packages=["pycord.multicog"],
-    version="2.1.0",
+    version="2.1.1",
     license="MIT",
     description="A pycord extension that allows splitting command groups into multiple cogs",
     long_description=readme,
